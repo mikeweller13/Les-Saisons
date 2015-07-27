4abd00adca13d29097bd2447dfa401c73b6baa21
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-07-27 12:10:24 -0700

    winter files placed and working. image files placed and working. Initial versions of autumn files not yet working, but instantiated.

---
+++ public/js/autumn.js
@@ -0,0 +1,13 @@
+// set up local storage variable
+
+// render b&w or color summer/winter based on whether puzzle was solved
+$('#summer').append('<img id=\"summer\" src=\"img/summer.png\"/>');
+$('#winter').append('<img id=\"autumn\" src=\"img/autumn.png\"/>');
+$('#winter').append('<img id=\"winter\" src=\"img/winter.png\"/>');
+$('#winter').append('<img id=\"spring\" src=\"img/spring.png\"/>');
+
+// if (summer){
+//   $('#summer').append('<img id=\"summer\" src=img/summer.png/>');
+// };
+
+// set up 
